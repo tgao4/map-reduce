46e415507e6762259d3fa993de5c8ebcc413d846
bunny <lgaouibht@gmail.com>
2018-07-11 21:52:22 -0400

    first commit

--- src/main/java/UnitMultiplication.java
+++ src/main/java/UnitMultiplication.java
@@ -11,6 +11,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class UnitMultiplication {
@@ -22,6 +23,17 @@ public class UnitMultiplication {
 
             //input format: fromPage\t toPage1,toPage2,toPage3
             //target: build transition matrix unit -> fromPage\t toPage=probability
+
+            String[] line = value.toString().trim().split("\t");
+            if (line.length == 1 || line[1].trim().equals("")) {
+                return;
+            }
+            String outputKey = line[0];
+            String[] pages = line[1].split(",");
+            double weight = (double)1 / pages.length;
+            for (String page : pages) {
+                context.write(new Text(outputKey), new Text(page + "=" + weight));
+            }
         }
     }
 
@@ -32,11 +44,19 @@ public class UnitMultiplication {
 
             //input format: Page\t PageRank
             //target: write to reducer
+            String[] pr = value.toString().trim().split("\t");
+            context.write(new Text(pr[0]), new Text(pr[1]));
         }
     }
 
     public static class MultiplicationReducer extends Reducer<Text, Text, Text, Text> {
+        float beta;
 
+        @Override
+        public void setup(Context context) {
+            Configuration conf = context.getConfiguration();
+            beta = conf.getFloat("beta", 0.2f);
+        }
 
         @Override
         public void reduce(Text key, Iterable<Text> values, Context context)
@@ -44,16 +64,35 @@ public class UnitMultiplication {
 
             //input key = fromPage value=<toPage=probability..., pageRank>
             //target: get the unit multiplication
+            HashMap<String, Double> map = new HashMap<String, Double>();
+            double prValue = 0;
+            for (Text value : values) {
+                if (value.toString().contains("=")) {
+                    String to = value.toString().split("=")[0];
+                    double prob = Double.parseDouble(value.toString().split("=")[1]);
+                    map.put(to, prob);
+                }
+                else {
+                    prValue = Double.parseDouble(value.toString());
+                }
+            }
+            for (String outputKey : map.keySet()) {
+                double outputValue = map.get(outputKey) * prValue * (1-beta);
+                context.write(new Text(outputKey), new Text(String.valueOf(outputValue)));
+            }
         }
     }
 
     public static void main(String[] args) throws Exception {
 
         Configuration conf = new Configuration();
+        conf.setFloat("beta", Float.parseFloat(args[3]));
         Job job = Job.getInstance(conf);
         job.setJarByClass(UnitMultiplication.class);
 
         //how chain two mapper classes?
+        ChainMapper.addMapper(job, TransitionMapper.class, Object.class, Text.class, Text.class, Text.class, conf);
+        ChainMapper.addMapper(job, PRMapper.class, Object.class, Text.class, Text.class, Text.class, conf);
 
         job.setReducerClass(MultiplicationReducer.class);
 
--- src/main/java/UnitSum.java
+++ src/main/java/UnitSum.java
@@ -5,7 +5,10 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.chain.ChainMapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
@@ -19,6 +22,29 @@ public class UnitSum {
 
             //input format: toPage\t unitMultiplication
             //target: pass to reducer
+
+            String[] line = value.toString().split("\t");
+            double subRank = Double.parseDouble(line[1]);
+            context.write(new Text(line[0]), new DoubleWritable(subRank));
+        }
+    }
+
+    public static class BetaMapper extends Mapper<Object, Text, Text, DoubleWritable> {
+
+        float beta;
+        @Override
+        public void setup(Context context) {
+            Configuration conf = context.getConfiguration();
+            beta = conf.getFloat("beta", 0.2f);
+        }
+
+        @Override
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
+
+            String[] line = value.toString().split("\t");
+            double weight = Double.parseDouble(line[1]) * beta;
+            context.write(new Text(line[0]), new DoubleWritable(weight));
         }
     }
 
@@ -30,20 +56,35 @@ public class UnitSum {
 
            //input key = toPage value = <unitMultiplication>
             //target: sum!
+            double sum = 0;
+            for (DoubleWritable value: values) {
+                sum += value.get();
+            }
+            DecimalFormat df = new DecimalFormat("#.0000");
+            sum = Double.valueOf(df.format(sum));
+            context.write(key, new DoubleWritable(sum));
         }
     }
 
     public static void main(String[] args) throws Exception {
 
         Configuration conf = new Configuration();
+        conf.setFloat("beta", Float.parseFloat(args[3]));
         Job job = Job.getInstance(conf);
         job.setJarByClass(UnitSum.class);
-        job.setMapperClass(PassMapper.class);
+
+        ChainMapper.addMapper(job, PassMapper.class, Object.class, Text.class, Text.class, DoubleWritable.class, conf);
+        ChainMapper.addMapper(job, BetaMapper.class, Text.class, DoubleWritable.class, Text.class, DoubleWritable.class, conf);
+
         job.setReducerClass(SumReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(DoubleWritable.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, PassMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, BetaMapper.class);
+
+
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
         job.waitForCompletion(true);
     }
 }
